fe60d396ea5f6652ffecb69dc86bfcbb75ef05fd
shashank <you@example.com>
2019-11-16 21:09:49 +0530

    redundant brancks

---
+++ redundantBracket.js
@@ -0,0 +1,33 @@
+/*
+Given a string of balanced expression, find if it contains a redundant parenthesis or not.
+A set of parenthesis are redundant if same sub-expression is surrounded by unnecessary or
+ multiple brackets. Print ‘Yes’ if redundant else ‘No’.
+Input: 
+((a+b))
+(a+(b)/c)
+(a+b*(c-d))
+Output: 
+Yes
+Yes
+No
+*/
+
+const redundantBracket = str => {
+  let stack = [];
+  for (let char of str) {
+    if (char == ")") {
+      let top = stack.pop();
+      let ans = true;
+      while (top != "(") {
+        if (top == "+" || top == "-" || top == "*" || top == "/") ans = false;
+        top = stack.pop();
+      }
+      if (ans == true) {
+        return 'YES';
+      }
+    } else stack.push(char);
+  }
+  return 'NO';
+};
+
+console.log(redundantBracket("((a+b))"));
